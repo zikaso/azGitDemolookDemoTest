bd4fcb7ab3b252a8664d43decc86b7061100d482
Zakaria Azizi <azizi.zakaria@gmail.com>
2021-01-01 12:54:28 +0100

    new update tow in the same last file

---
+++ BO/src/main/java/com/az/dev/Poduct.java
@@ -0,0 +1,40 @@
+package com.az.dev;
+
+public class Poduct {
+    private  Long id;
+    private String name;
+    private String tel;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
